7d844f00d4154cbf16be789f3ae9b6dbca788d4a
Betsy <Betsy@123.com>
2023-12-25 15:26:21 +0800

    second commit

--- src/main/java/com/betsy/GitTest.java
+++ src/main/java/com/betsy/GitTest.java
@@ -3,5 +3,6 @@ package com.betsy;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git1!");
     }
 }
