e7028686d066573b844d2be72f38b0d69a05e5ee
Betsy <Betsy@123.com>
2023-12-25 16:31:42 +0800

    master commit2

--- src/main/java/com/betsy/GitTest.java
+++ src/main/java/com/betsy/GitTest.java
@@ -2,7 +2,7 @@ package com.betsy;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git! Master");
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
