9b8c8a19b22d51d73cf2cd6ac9eeec0e40f21b19
Betsy <Betsy@123.com>
2023-12-25 16:35:24 +0800

    hot-fix test3

--- src/main/java/com/betsy/GitTest.java
+++ src/main/java/com/betsy/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello Git4!");
         System.out.println("Hot-Fix Test");
         System.out.println("Hot-Fix Test2");
+        System.out.println("Hot-Fix Test3");
     }
 }
