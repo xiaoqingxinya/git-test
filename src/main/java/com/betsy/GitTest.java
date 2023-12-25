12b233362fdf84b44cfeb62f494618941eee6f11
xiaoqingxinya <130819410+xiaoqingxinya@users.noreply.github.com>
2023-12-25 20:40:29 +0800

    pull commit

--- src/main/java/com/betsy/GitTest.java
+++ src/main/java/com/betsy/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Master Test2!");
         System.out.println("Hot-Fix Test3");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
